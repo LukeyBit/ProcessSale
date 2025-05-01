f68f34a738be8641213b78f7d7e8b8a9561d9bf2
LukeyBit <lukas@lukasandersson.com>
2025-05-01 12:49:32 +0200

    Commenting

--- src/model/Receipt.java
+++ src/model/Receipt.java
@@ -1,6 +1,17 @@
 package model;
 
+/**
+ * The Receipt class represents a receipt generated after a sale in the Point of Sale (POS) system.
+ * It contains information about the sale, including date, total amount, VAT, change, and items sold.
+ */
 public class Receipt {
+
+    /**
+     * Constructor for the Receipt class.
+     * It initializes the receipt with the sale information and prints it to the console.
+     *
+     * @param sale The sale object containing information about the sale.
+     */
     protected Receipt(Sale sale) {
         System.out.println(
                 "Receipt\n"
--- src/view/View.java
+++ src/view/View.java
@@ -4,9 +4,20 @@ import controller.Controller;
 
 import java.util.Scanner;
 
+/**
+ * The View class represents the user interface of the Point of Sale (POS) system.
+ * It interacts with the cashier to scan items, add customers, and process payments.
+ */
 public class View {
     private Controller controller;
 
+    /**
+     * Constructor for the View class.
+     * Takes a Controller object to interact with the system.
+     * Initializes the sale and prompts the user for input.
+     *
+     * @param controller The Controller object to interact with the system.
+     */
     public View(Controller controller) {
         this.controller = controller;
         controller.startSale();
@@ -46,18 +57,37 @@ public class View {
         endSale();
     }
 
+    /**
+     * Scans an item and adds it to the sale.
+     *
+     * @param itemIdentifier The identifier of the item to be scanned.
+     * @param quantity The quantity of the item to be scanned.
+     */
     public void scanItem(int itemIdentifier, int quantity) {
         controller.enterItem(itemIdentifier, quantity);
     }
 
+    /**
+     * Adds a customer to the sale.
+     *
+     * @param customerIdentifier The identifier of the customer to be added.
+     */
     public void addCustomer(int customerIdentifier) {
         controller.fetchCustomer(customerIdentifier);
     }
 
+    /**
+     * Processes the payment for the sale.
+     *
+     * @param payment The amount of payment to be processed.
+     */
     public void pay(float payment) {
         controller.enterPayment(payment);
     }
 
+    /**
+     * Ends the sale by calling the controller's endSale method.
+     */
     public void endSale() {
         controller.endSale();
     }
